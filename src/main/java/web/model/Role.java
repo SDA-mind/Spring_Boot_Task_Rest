48dbb6b1d26d225efbbe7bc941e9762c4c4a4cfa
SDA-mind <dima-sherbakov@mail.ru>
2020-09-15 17:01:12 +0300

    дфые

--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -61,4 +61,9 @@ public class Role implements GrantedAuthority {
     public int hashCode() {
         return Objects.hash(getId(), getRole());
     }
+
+    @Override
+    public String toString() {
+        return role.substring(5);
+    }
 }
